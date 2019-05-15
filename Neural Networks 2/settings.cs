9454ad2b45fb23cc9a1806bae950fa88031111e1
Piotr Kruszewski <32763677+TheNishishiro@users.noreply.github.com>
2019-05-15 20:06:40 +0200

    Commented settings
    Added comments to settings

--- Neural Networks 2/settings.cs
+++ Neural Networks 2/settings.cs
@@ -16,15 +16,16 @@ namespace Neural_Networks_2
         public static List<float> avaragefitnessHistory = new List<float>();
 
         public static int Generation = 0;
-        public static int Population = 20;
-        public static int timeLimit = 2500;
-        public static int maxFood = 50;
-        public static int mutation = 1;
-        public static float initialHP = 255;
-        public static float maxHP = 500;
-        public static float HPdrain = 0.9f;
-        public static float HPrecover = 30;
-        public static int mutationRange = 100;
+        public static int Population = 20;      // Number of flies in generation
+        public static int timeLimit = 2500;     // simulation time in frames 
+        public static int maxFood = 50;         // Number of food spawns per map
+        public static int mutation = 1;         // Mutation rate in %
+        public static float initialHP = 255;    // HP at which flies start life with, decreases if fly doesn't eat 
+        public static float maxHP = 500;        // HP threshold at which it dies from eating too much 
+        public static float HPdrain = 0.9f;     // HP drop for each frame of life
+        public static float HPrecover = 30;     // HP recovered per food eaten
+        public static int mutationRange = 100;  // How much can neural networks weights change if mutation happens (divided by 100)
+
         public static Rectangle cursor;
 
         public static Random rnd = new Random();
